64a512b4143bab5fdc5442afe1a9b4fd2e3e722a
esegala <esegala@deloitte.co.uk>
2022-01-01 16:43:50 +0000

    improving physical commands

--- src/components/PhysicalMovements.js
+++ src/components/PhysicalMovements.js
@@ -1,13 +1,13 @@
 import useLongPress from "../hooks/useLongPress";
 
 const PhysicalMovements = () => {
-  const onKeyLongPress = (type) => {
+  const onKeyLongPress = (type, key, keyCode, code) => {
     document.dispatchEvent(
       new KeyboardEvent(type, {
-        key: "W",
-        keyCode: 87,
-        code: "KeyW",
-        which: 87,
+        key: key,
+        keyCode: keyCode,
+        code: code,
+        which: keyCode,
         shiftKey: false,
         ctrlKey: false,
         metaKey: false,
@@ -15,17 +15,53 @@ const PhysicalMovements = () => {
     );
   };
 
-  const longWPressEvent = useLongPress(() => onKeyLongPress("keydown"), null, {
-    shouldPreventDefault: true,
-    delay: 0,
-    onClear: () => onKeyLongPress("keyup"),
-  });
+  const longWPressEvent = useLongPress(
+    () => onKeyLongPress("keydown", "W", 87, "KeyW"),
+    null,
+    {
+      shouldPreventDefault: true,
+      delay: 0,
+      onClear: () => onKeyLongPress("keyup", "W", 87, "KeyW"),
+    }
+  );
+
+  const longSPressEvent = useLongPress(
+    () => onKeyLongPress("keydown", "S", 83, "KeyS"),
+    null,
+    {
+      shouldPreventDefault: true,
+      delay: 0,
+      onClear: () => onKeyLongPress("keyup", "S", 83, "KeyS"),
+    }
+  );
+
+  const longAPressEvent = useLongPress(
+    () => onKeyLongPress("keydown", "A", 65, "KeyA"),
+    null,
+    {
+      shouldPreventDefault: true,
+      delay: 0,
+      onClear: () => onKeyLongPress("keyup", "A", 65, "KeyA"),
+    }
+  );
+
+  const longDPressEvent = useLongPress(
+    () => onKeyLongPress("keydown", "D", 68, "KeyD"),
+    null,
+    {
+      shouldPreventDefault: true,
+      delay: 0,
+      onClear: () => onKeyLongPress("keyup", "D", 68, "KeyD"),
+    }
+  );
 
   return (
-    <>
+    <div className="controls">
       <button {...longWPressEvent}>UP</button>
-      <button {...longWPressEvent}>DOWN</button>
-    </>
+      <button {...longSPressEvent}>DOWN</button>
+      <button {...longDPressEvent}>RIGHT</button>
+      <button {...longAPressEvent}>LEFT</button>
+    </div>
   );
 };
 
