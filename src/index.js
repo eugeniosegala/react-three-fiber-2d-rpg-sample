9ae535bcd823ee4134d04b9fa60978af8ae67ec4
eugeniosegala <eugenio.segala@hotmail.it>
2022-01-07 23:29:37 +0000

    adding loader

--- src/index.js
+++ src/index.js
@@ -2,15 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/cannon";
+import { useProgress } from "@react-three/drei";
 
 import SampleLevel from "./levels/SampleLevel";
 import PhysicalMovements from "./components/PhysicalMovements";
 
 import "./index.css";
 
+function Loader() {
+  const { progress } = useProgress();
+  return <div>loading {progress.toFixed()} %</div>;
+}
+
 const Game = () => {
   return (
     <>
+      <Loader />
       <PhysicalMovements />
       <Canvas orthographic camera={{ zoom: 50, position: [0, 5, 0] }}>
         <Physics
